6ee2be45a5f09662d41946e4f7eafd90d85ba67a
Bubblehead333 <joshlyell@live.co.uk>
2019-10-26 13:06:56 +0100

    -Old data persists on sign up if another field was entered incorrectly

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -66,6 +66,8 @@ $container['AuthenticationController'] = function($container) {
 };
 
 $app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
+$app->add(new \App\Middleware\OldInputMiddleware($container));
+
 
 
 require __DIR__ . '/../app/routes.php';
